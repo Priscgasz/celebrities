001f1dd803f378c8350f4c224f51e5156819713d
Priscilla Gasztowtt <pgasztowtt@SIA-Conseil.local>
2021-11-03 16:45:42 +0100

    first commit

---
+++ models/Celebrity.model.js
@@ -0,0 +1,10 @@
+const { Schema, model } = require("mongoose");
+
+const celebritySchema = new Schema({
+  name: String,
+  occupation: String,
+  catchPhrase: String,
+});
+
+const celebrityModel = model("celebrities", celebritySchema);
+module.exports = celebrityModel;
